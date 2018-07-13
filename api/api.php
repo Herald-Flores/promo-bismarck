35fdcf57df8f59bf6a2e3aacc551d6dcb3c4f64c
Herald-Flores <heraldflores95@gmail.com>
2018-07-13 09:13:54 -0600

    nuevas mejoras en el responsive

---
+++ api/api.php
@@ -0,0 +1,68 @@
+<?php
+    echo 'angora si';
+    //die();
+    header("Content-type: application/json");
+    
+        // $objDatos = json_decode(file_get_contents("php://input"));
+    // $name = @trim(stripslashes($objDatos->name));
+    // $email = @trim(stripslashes($objDatos->email));
+    // $phone = @trim(stripslashes($objDatos->phone));
+    // $message = @trim(stripslashes($objDatos->msj));
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $company = $_POST['Company'];
+    $proyect = $_POST['proyect'];
+    echo $name;
+
+require 'class.phpmailer.php';
+$mail=new PHPMailer();
+$mail->CharSet = 'UTF-8';
+
+//$body = 'Correo: '.$email.' Empresa:  '.$phone.' Mensaje:  '.$message;
+
+$body = <<<EOT
+Titular: $name <br>
+Empresa: $company <br>
+Correo: $email <br>
+
+-------------------------------------------<br>
+<br>
+$proyect
+EOT;
+
+$mail->IsSMTP();
+$mail->Host       = 'smtp.gmail.com';
+
+$mail->SMTPSecure = 'tls';
+$mail->Port       = 587;
+$mail->SMTPDebug  = 1;
+$mail->SMTPAuth   = true;
+
+$mail->Username   = 'bmarkt.studio@gmail.com';
+$mail->Password   = 'bmarkt162216';
+
+$mail->SetFrom($email, $name);
+$mail->AddReplyTo('bmarkt.studio@gmail.com','no-reply');
+$mail->Subject    = 'Aplicacion para campania El punto de Partida';
+$mail->MsgHTML($body);
+
+
+$mail->AddAddress('heraldflores95@gmail.com', 'Herald');
+// $mail->AddAddress('marvingutierrezjr@gmail.com', 'Marvin');
+// $mail->AddAddress('jenniercruz90@gmail.com', 'Jennier');
+//$mail->AddAddress('contacto@bigbangnica.studio', 'BigBang Studio');
+// $mail->AddAddress('duviedh22@gmail.com', 'Jerson');
+// $mail->AddAddress('bahrdiseno@gmail.com', 'Byron');
+
+echo $body;
+//die();
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    header("Location: https://bigbang.studio/USA/gracias.html");
+    die();
+    echo 'Message has been sent';
+}
+
+?>
